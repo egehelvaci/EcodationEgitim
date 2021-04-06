e69d4e645ac09a40f72247f3141b843f26573c5d
egehelvaci <egehelvaci@googlemail.com>
2021-04-06 20:22:59 +0300

    initial commit

---
+++ src/com/ecodation/odev/Odev_003.java
@@ -0,0 +1,14 @@
+package com.ecodation.odev;
+
+public class Odev_003 {
+
+	// 1.sayı: 20  2.sayı: 10 ==> 
+	//2 sayının:  toplamı/çıkarması/bölmesi/çarpımı
+	public static void main(String[] args) {
+		int x7 = 0x52;
+		System.out.println(x7);
+		
+
+	}
+
+}
